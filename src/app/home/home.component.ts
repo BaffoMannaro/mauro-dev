4ca22a566387de3065fe702339203a1728f23325
Mauro Altamura <altamura.mauro@gmail.com>
2024-07-29 15:54:41 +0200

    Formattazione home terminata

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
 import { CommonModule } from '@angular/common';
 import { WeatherComponent } from '../weather/weather.component';
@@ -8,7 +8,7 @@ import { FooterComponent } from '../footer/footer.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, CommonModule, WeatherComponent, FooterComponent],
+  imports: [RouterLink, NavBarComponent, CommonModule, WeatherComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -25,8 +25,23 @@ export class HomeComponent implements OnInit{
     'SEO'
   ];
 
+  selectedService1: boolean = false;
+  selectedService2: boolean = false;
+  selectedService3: boolean = false;
+
   constructor() {}
 
   ngOnInit(): void {}
 
+  onServiceClick(i:number): void{
+    if(i === 1){
+      this.selectedService1 = !this.selectedService1;
+    }
+    else if (i === 2){
+      this.selectedService2 = !this.selectedService2;
+    }
+    else if(i === 3){
+      this.selectedService3 = !this.selectedService3;
+    }
+  }
 }
--- src/app/services/seo/seo.component.ts
+++ src/app/services/seo/seo.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../footer/footer.component';
+import { NavBarComponent } from '../../nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-seo',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent, FooterComponent],
   templateUrl: './seo.component.html',
   styleUrl: './seo.component.scss'
 })

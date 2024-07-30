76bcae52dfc4cacf920cca44a523ae2cdc3f8399
Mauro Altamura <altamura.mauro@gmail.com>
2024-07-30 13:30:12 +0200

    link crati in pagina SEO

--- src/app/services/seo/seo.component.ts
+++ src/app/services/seo/seo.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../footer/footer.component';
 import { NavBarComponent } from '../../nav-bar/nav-bar.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-seo',
   standalone: true,
-  imports: [NavBarComponent, FooterComponent],
+  imports: [NavBarComponent, FooterComponent, RouterLink],
   templateUrl: './seo.component.html',
   styleUrl: './seo.component.scss'
 })

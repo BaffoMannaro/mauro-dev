b32557a17761701186eccc3596c0644dbd490d33
Mauro Altamura <altamura.mauro@gmail.com>
2024-07-31 19:33:40 +0200

    web-app pagina

--- src/app/services/web-app/web-app.component.ts
+++ src/app/services/web-app/web-app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../../nav-bar/nav-bar.component';
+import { FooterComponent } from '../../footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-web-app',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent, FooterComponent, RouterLink],
   templateUrl: './web-app.component.html',
   styleUrl: './web-app.component.scss'
 })

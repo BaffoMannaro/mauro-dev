e4b6f0b400855654b3f0c97e7e7b7ed47763798e
Mauro Altamura <altamura.mauro@gmail.com>
2024-08-27 16:40:18 +0200

    App meteo di base, funzionante

--- src/app/app-service/weather.service.ts
+++ src/app/app-service/weather.service.ts
@@ -1,21 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class WeatherService {
 
-  APIkey: string = "f5743e0b9ef8bdb08200a1d96540bf54";
+export class WeatherService {
+  API_KEY: string = "f5743e0b9ef8bdb08200a1d96540bf54";
+  API_URL: string = "https://api.openweathermap.org/data/2.5/weather?";
+  cityName: string = "Monopoli";
 
   constructor(private httpClient: HttpClient) {}
 
-  getWeather(lat: number, lon: number, APIkey:string){
-
-    APIkey = this.APIkey;
-    
-
+  get_weather(): Observable<any> {
+    return this.httpClient.get(`${this.API_URL}q=${this.cityName}&units=metric&lang=it&appid=${this.API_KEY}`);
   }
-
 
 }
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { DarkLightModeService } from '../app-service/dark-light-mode.service';
 import { CommonModule } from '@angular/common';
+import { WeatherService } from '../app-service/weather.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-weather',
@@ -9,8 +11,45 @@ import { CommonModule } from '@angular/common';
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.scss'
 })
-export class WeatherComponent {
+export class WeatherComponent{
+  private weather: any = [];
+  private weatherObservable: Observable<any[]>;
 
-  constructor( public DarkLightModeService: DarkLightModeService){}
+  // gestione info sul meteo
+  public weather_city: string = '';
+  public temperature: number = 0;
+  public weather_description: string = '';
+  public feels_like: number = 0;
+  public wind_speed: string = '';
+  public humidity: string = '';
+  public weather_icon: string = '';
+
+  // gestione della data di oggi
+  public today : number = Date.now();
+
+  // costruttore
+  constructor(
+    public DarkLightModeService: DarkLightModeService,
+    public weatherService: WeatherService,
+  ){
+
+    this.weatherObservable = this.weatherService.get_weather();
+
+    this.weatherService.get_weather().subscribe((res:any[]) => {
+      this.weather = res;
+
+      this.weather_city = this.weather.name.replace('Comune di ', '');
+      this.temperature = parseInt(this.weather.main.temp);
+      this.weather_description = this.weather.weather[0].description;
+      this.feels_like = parseInt(this.weather.main.feels_like);
+      this.wind_speed = this.weather.wind.speed;
+      this.humidity = this.weather.main.humidity;
+      this.weather_icon = this.weather.weather[0].icon;
+
+    })
+
+  }
+
+
 
 }

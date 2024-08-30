b38ff0144558d5c2eefa102f32436922ca385590
Mauro Altamura <altamura.mauro@gmail.com>
2024-08-30 18:35:31 +0200

    Responsive Navbar - ultimata

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { DarkLightModeService } from '../app-service/dark-light-mode.service';
 import { CommonModule } from '@angular/common';
@@ -10,8 +10,51 @@ import { CommonModule } from '@angular/common';
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+
+  public isLargeScreen: boolean = false;
+  public isMenuActive: boolean = false;
+  public menu_icon: string = "menu";
+  public menu_mode: string = '';
+
+  ngOnInit() {
+    this.checkScreenSize();
+  }
 
   constructor(public DarkLightModeService: DarkLightModeService){}
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    this.isLargeScreen = window.innerWidth < 1100;
+    this.isMenuActive = window.innerWidth < 1100;
+  }
+
+  openMenu(){
+    if(this.isLargeScreen == false){
+      this.isLargeScreen = true;
+      this.menu_icon = "menu";
+    }
+    else{
+      this.isLargeScreen = false;
+      this.menu_icon = "menu-x";
+    }
+
+    this.menu_mode = 'Light mode';
+
+  }
+
+  switchMode(){
+    if(this.menu_mode === 'Light mode'){
+      this.menu_mode = 'Dark mode';
+    }
+    else if(this.menu_mode === 'Dark mode'){
+      this.menu_mode = 'Light mode';
+    }
+  }
+
 
 }

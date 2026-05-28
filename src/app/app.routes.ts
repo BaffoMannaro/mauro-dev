513810ec2df91c27b3660192ccec9647f7a3c562
Mauro Altamura <altamura.mauro@gmail.com>
2026-05-28 12:10:15 +0200

    DOWN

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,32 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ServicesComponent } from './services/services.component';
-import { SeoComponent } from './services/seo/seo.component';
-import { WebComponent } from './services/web/web.component';
-import { LandingComponent } from './services/landing/landing.component';
-import { BlogComponent } from './services/blog/blog.component';
-import { ECommerceComponent } from './services/e-commerce/e-commerce.component';
-import { DesignComponent } from './services/design/design.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { WebAppComponent } from './services/web-app/web-app.component';
 
 export const routes: Routes = [
-
-  {'path' : '', component: HomeComponent},
-  {'path' : 'home', component:HomeComponent},
-
-  {'path' : 'services', component:ServicesComponent},
-  {'path' : 'services/seo', component:SeoComponent},
-  {'path' : 'services/web', component:WebComponent},
-  {'path' : 'services/landing-page', component:LandingComponent},
-  {'path' : 'services/blog', component:BlogComponent},
-  {'path' : 'services/e-commerce', component:ECommerceComponent},
-  {'path' : 'services/web-app', component:WebAppComponent},
-  {'path' : 'services/design', component:DesignComponent},
-
-  {'path' : 'about', component:AboutComponent},
-
-  {'path' : 'contacts', component:ContactComponent}
-
+  { path: '', component: HomeComponent },
+  { path: '**', redirectTo: '' }
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,58 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { NavBarComponent } from "../nav-bar/nav-bar.component";
 import { CommonModule } from '@angular/common';
-import { WeatherComponent } from '../weather/weather.component';
-import { FooterComponent } from '../footer/footer.component';
+import { Component } from '@angular/core';
 import { DarkLightModeService } from '../app-service/dark-light-mode.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, NavBarComponent, CommonModule, WeatherComponent, FooterComponent],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent {
+  readonly contactEmail = 'altamura.mauro@gmail.com';
+  readonly currentYear = new Date().getFullYear();
 
-  words: string[] = [
-    'Branding',
-    'User Interface',
-    'Web-App',
-    'E-Commerce',
-    'Blog',
-    'Wire Framing',
-    'User Experience',
-    'SEO'
-  ];
+  constructor(public DarkLightModeService: DarkLightModeService) {}
 
-  selectedService1: boolean = false;
-  selectedService2: boolean = false;
-  selectedService3: boolean = false;
-  public cmsFlag: boolean = true;
-  public nerdStatsFlag: boolean = false;
-
-  constructor( public DarkLightModeService: DarkLightModeService){}
-
-  ngOnInit(): void {}
-
-  onServiceClick(i:number): void{
-    if(i === 1){
-      this.selectedService1 = !this.selectedService1;
-    }
-    else if (i === 2){
-      this.selectedService2 = !this.selectedService2;
-    }
-    else if(i === 3){
-      this.selectedService3 = !this.selectedService3;
-    }
-  }
-
-  closeCms(){
-    this.cmsFlag = false;
-  }
-
-  switchNerdStats(){
-    this.nerdStatsFlag =! this.nerdStatsFlag;
+  reload(): void {
+    window.location.reload();
   }
 }
